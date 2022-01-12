1ed1eea92a52f79add8f96c3530ae1b3a948faab
prasanna <prasanna.k.r@accenture.com>
2022-01-12 19:54:44 +0530

    added devise to the application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_121721) do
+ActiveRecord::Schema.define(version: 2022_01_12_122737) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_date"
@@ -68,6 +68,18 @@ ActiveRecord::Schema.define(version: 2022_01_11_121721) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["classroom_id"], name: "index_students_on_classroom_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "appointments", "patients"
